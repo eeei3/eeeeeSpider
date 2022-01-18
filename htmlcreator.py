68ab1bb4d8f4ba37ed7ea4f7fb5608c13c3fac86
eeei3 <38956367+eeei3@users.noreply.github.com>
2022-01-18 11:18:22 -0600

    disabled some linting stuff

--- htmlcreator.py
+++ htmlcreator.py
@@ -1,5 +1,7 @@
 #! /usr/local/bin/python3
 
+# pylint: disable=C0116
+
 """
 # /***************************************************************************************
 #  This module is responsible for generating an HTML file from the given links.
