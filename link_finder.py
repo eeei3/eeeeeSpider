829e61e88b125b3f122ba45f61f39cad6a65869f
eeei3 <38956367+eeei3@users.noreply.github.com>
2022-01-18 11:17:58 -0600

    disabled some linting stuff

--- link_finder.py
+++ link_finder.py
@@ -1,5 +1,7 @@
 #! /usr/local/bin/python3
 
+# pylint: disable=C0116
+
 """
 # /***************************************************************************************
 #  This module is responsible for turning snippits of HTML into actual links.
