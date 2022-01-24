51612ecb6e8ef40e3c019c0bbc3aed70ca210490
eeei3 <38956367+eeei3@users.noreply.github.com>
2022-01-23 18:21:28 -0600

    more consistent spacing

--- domain.py
+++ domain.py
@@ -10,19 +10,27 @@ from urllib.parse import urlparse
 
 # Getting the domain name ex. github
 def get_domain_name(url):
+
     try:
+
         # The results returned by get_sub_domain_name()
         results = get_sub_domain_name(url).split('.')
         return str(results[-2]) + '.' + str(results[-1])
+
     except Exception as error:
+
         print(error)
         return ''
 
 
 # Getting the sub domain name ex. github.com
 def get_sub_domain_name(url):
+
     try:
+
         return urlparse(url).netloc
+
     except Exception as error:
+
         print(error)
         return ''
